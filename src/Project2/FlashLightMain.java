a953289b0c3547aac86ab34a46cbd9d0663e4ea0
Bright-nut <bestones00005@gmail.com>
2023-03-18 23:30:03 -0400

    Homework number 13 continued

---
+++ src/Project2/FlashLightMain.java
@@ -0,0 +1,12 @@
+package Project2;
+
+public class FlashLightMain {
+    public static void main(String[] args) {
+        FlashLight fl = new FlashLight();
+        fl.turnOff();
+        fl.turnOn();
+
+
+
+    }
+}
