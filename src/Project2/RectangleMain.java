8ba85577088becc77933ab0c728588227a5862f9
Bright-nut <bestones00005@gmail.com>
2023-03-18 23:31:34 -0400

    Home Work # 13 section b continued

---
+++ src/Project2/RectangleMain.java
@@ -0,0 +1,10 @@
+package Project2;
+
+public class RectangleMain {
+    public static void main(String[] args) {
+        Rectangle rec = new Rectangle(2.3,1.3);
+
+        System.out.println(rec.printArea());
+
+    }
+}
